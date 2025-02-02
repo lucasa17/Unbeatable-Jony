226d249c131ed126a7df4d358deb0f24f0b9eeab
Luis-Ricardo-Carvalho <luisricardocarvalholin@gmail.com>
2025-02-02 09:07:19 -0300

    Update inimigo_spawner.gd

--- Jogo/Cenas/inimigo_spawner.gd
+++ Jogo/Cenas/inimigo_spawner.gd
@@ -25,31 +25,28 @@ func _on_timer_timeout():
 		#pega um local aleatorio par spawnar o inimigo
 		var spawn = spawn_points[randi() % spawn_points.size()]
 		#spawna um inimigo
-		if cont <= 4  :
+		if Fase01.wave <= 4  :
 			var goblin = goblin_scene.instantiate()
 			goblin.position = spawn.position
 			goblin.hit_player.connect(hit)
 			main.add_child(goblin)
 			goblin.add_to_group("enemies")
 			
-		if cont > 4 and cont <= 15:
+		if Fase01.wave > 4 and Fase01.wave <= 8:
 			var goblin = goblin_scene2.instantiate()
 			goblin.position = spawn.position
 			goblin.hit_player.connect(hit)
 			main.add_child(goblin)
 			goblin.add_to_group("enemies")
 			
-		if cont > 15:
+		if cont > 8:
 			var goblin = goblin_scene3.instantiate()
 			goblin.position = spawn.position
 			goblin.hit_player.connect(hit)
 			main.add_child(goblin)
 			goblin.add_to_group("enemies")
 			
-	if enemies.size() == get_parent().max_enemies:
-		cont = cont+1
 
 func  hit():
 	hit_p.emit()
-	if enemies.size() < get_parent().max_enemies:
-		cont = cont-1
+	
